7658765b567738d9401c34741fd11da2b5ddf046
Nicholas Benedict <nicholasdbenedict@gmail.com>
2021-09-30 09:08:48 -0400

    Add project files.

---
+++ HangMan/CorrectGuess.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan
+{
+    public class CorrectGuess
+    {
+        public void CorrectGuessDisplay(string word, int numOfLives)
+        {
+            Console.Clear();
+            word = word.Replace(" ", string.Empty);
+            char[] wordToBeGuessed = new char[word.Length];
+
+            int numberOfGuesses = numOfLives;
+            string holder = "";
+            
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordToBeGuessed[i] = '_';
+            }
+
+            holder = new string(wordToBeGuessed);
+            Console.WriteLine($"The word you are guessing is {holder}");
+            Console.Write("Please enter your guess: \n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            while (holder != word && numberOfGuesses > 0)
+            {
+                char guess = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (word.Contains(guess))
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (guess == word[i])
+                        {
+                            wordToBeGuessed[i] = guess;
+                            holder = new string(wordToBeGuessed);
+                        }
+                    } 
+                    Console.WriteLine(holder + "\n");
+                    Console.WriteLine("Guess the next letter");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Sorry that was a wrong guess \n");
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    numberOfGuesses--;
+                    Console.WriteLine($"You have {numberOfGuesses} lives left");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            }
+            if (numberOfGuesses == 0)
+            {
+                Console.WriteLine("You lost the game!");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"The word you were trying to guess was {word}");
+                Console.ReadLine();
+            }
+            else if(numberOfGuesses > 0)
+            {
+                Console.WriteLine("Congrats on guessing the word!");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ HangMan/HangManMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace HangMan
+{
+    class HangManMain
+    {
+        static void Main(string[] args)
+        {
+            CorrectGuess correctGuess = new CorrectGuess();
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("HangMan.dictionary.txt");
+            StreamReader reader = new StreamReader(stream);
+            string file2 = reader.ReadToEnd();
+            string[] readFile = file2.Split('\n');
+
+            Console.WriteLine("Welcome to HangMan \n");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("One or Two Players (1 or 2): ");
+            int numOfPlayers = Convert.ToInt32(Console.ReadLine());
+
+            if (numOfPlayers == 1)
+            {
+                Random random = new Random();
+                int wordNum = random.Next(0, readFile.Length);
+                string word = readFile[wordNum];
+                correctGuess.CorrectGuessDisplay(word, 10);
+            }
+            else
+            {
+                Console.WriteLine("Enter the word or words you want the other person to guess: \n");
+                Console.ForegroundColor = ConsoleColor.Green;
+                string word = Console.ReadLine();
+                Console.WriteLine("How many guesses do they get?(please enter a number): \n");
+                int numberOfLives = Convert.ToInt32(Console.ReadLine());
+                
+                correctGuess.CorrectGuessDisplay(word, numberOfLives);
+            }
+
+        }
+    }
+}
